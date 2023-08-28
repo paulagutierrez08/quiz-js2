c358071b81b69f832d3fe05a5f383ae3afc94e28
Usuario <usuario@MacBook-Pro-de-Usuario.local>
2023-08-28 12:16:04 -0500

    quiz-js2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-/**
- * This quiz is to test your knowledge of if-statements.
- *
- * 1. Create a repository by cloning: https://github.com/jorgecarmona/npm-project-template
- * 2. Follow the README for instructions
- * 3. When you finish the quiz, create a PR on you repository
- * 4. Submit to your lead
- */
+// /**
+//  * This quiz is to test your knowledge of if-statements.
+//  *
+//  * 1. Create a repository by cloning: https://github.com/jorgecarmona/npm-project-template
+//  * 2. Follow the README for instructions
+//  * 3. When you finish the quiz, create a PR on you repository
+//  * 4. Submit to your lead
+//  */
 
 /**
  * Question 1
